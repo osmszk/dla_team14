a6253f95507a3e1e830d7ebe98fbae36708b4e24
osuzukix <osuzuki@plegineer.com>
2017-11-21 00:05:07 +0900

    add keras(CNN)

---
+++ keras/szk_input.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+
+import csv
+import numpy as np
+from PIL import Image, ImageTk
+
+def main():
+    (X_train, Y_train) = read_data('./data/train/data.txt')
+    (imgs_test, labels_test) = read_data('./data/test/data.txt')
+    print(X_train.shape)
+    print(Y_train.shape)
+
+def read_data(path):
+    imgs = []
+    labels = []
+    f = open(path, 'r')
+    dataReader = csv.reader(f, delimiter=' ')
+    for row in dataReader:
+        path = row[0]
+        img = Image.open(path, 'r').resize((112,112))
+        img = np.asarray(img)
+        imgs.append(img)
+        label = row[1]
+        labels.append(label)
+    return (np.array(imgs), np.array(labels).reshape(-1,1))
