97dc9106f3d7dc31cdf505f5e3b2760a4e903301
osuzukix <osuzuki@plegineer.com>
2017-12-02 16:24:11 +0900

    fix devide images func

--- devide_test_train.rb
+++ devide_test_train.rb
@@ -12,10 +12,10 @@ talents.each do |dir|
   FileUtils.mkdir_p(train_image_dir+dir) unless FileTest.exist?(train_image_dir+dir)
   FileUtils.mkdir_p(test_image_dir+dir) unless FileTest.exist?(test_image_dir+dir)
 
-  sum = Dir.glob("./member_images/cropped_image_#{dir}/*.png").count
-  p "sum of #{dir}:#{sum.to_s}"
+  sum = Dir.glob("./member_images/cropped_images_#{dir}/*.png").count
+  puts "sum of #{dir}:#{sum.to_s}"
 
-  Dir.glob("./member_images/cropped_image_#{dir}/*.png").each_with_index do |src, index|
+  Dir.glob("./member_images/cropped_images_#{dir}/*.png").each_with_index do |src, index|
     percent = index.to_f/sum.to_f
     if percent < RATIO_TRAIN_DATA
       FileUtils.cp(src, train_image_dir+dir+"/")
@@ -24,8 +24,8 @@ talents.each do |dir|
     end
   end
 
-  p "train:"
-  p Dir.glob("./#{train_image_dir+dir}/*.png").count
-  p "test:"
-  p Dir.glob("./#{test_image_dir+dir}/*.png").count
+  puts "train:"
+  puts Dir.glob("./#{train_image_dir+dir}/*.png").count
+  puts "test:"
+  puts Dir.glob("./#{test_image_dir+dir}/*.png").count
 end
