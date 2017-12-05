24aac758616e3697ce5e0a58d2f45e92e43a4f4f
osuzukix <osuzuki@plegineer.com>
2017-12-05 23:24:14 +0900

    add command-option of floydhub

--- keras/szk_input.py
+++ keras/szk_input.py
@@ -4,11 +4,17 @@ import csv
 import numpy as np
 import os
 from PIL import Image, ImageTk
+import sys
+
+# -f floydhubで実行する場合は、`-f`オプションをつける。
+on_floydhub = True if "-f" in sys.argv else False
 
 def main():
-    #TODO: for floydhub ./data から /data に変更した。あとでparamで動的にする
-    (X_train, Y_train) = read_data('/data/train/data.txt')
-    (imgs_test, labels_test) = read_data('/data/test/data.txt')
+    #for floydhub ./data から /data に変更
+    train_data_path = '/data/train/data.txt' if on_floydhub else './data/train/data.txt'
+    test_data_path = '/data/test/data.txt' if on_floydhub else './data/test/data.txt'
+    (X_train, Y_train) = read_data(train_data_path)
+    (imgs_test, labels_test) = read_data(test_data_path)
     print(X_train.shape)
     print(Y_train.shape)
 
@@ -19,8 +25,10 @@ def read_data(path):
     dataReader = csv.reader(f, delimiter=' ')
     for row in dataReader:
         path = row[0]
-        #TODO: for floydhub ./data から /data に変更した。あとでparamで動的にする
-        path = path[1:]
+        if on_floydhub:
+            #for floydhub ./data から /data に変更
+            path = path[1:]
+
         if not os.path.exists(path):
             continue
         img = Image.open(path, 'r').resize((112,112))
